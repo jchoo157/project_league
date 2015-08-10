c839df7733af6f3e87792b29f57a141b895fbbb5
Johnny <jchoo157@gmail.com>
2015-08-10 18:23:27 -0400

    Testing images

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -18,21 +18,41 @@ class PagesController < ApplicationController
 
   def match_history
     id
-    @match_history = JSON.parse(RestClient.get"https://na.api.pvp.net/api/lol/na/v2.2/matchhistory/#{@id}?api_key=45ff6a4c-f994-420d-a018-189db4cb494e")
+    @match_history = JSON.parse(RestClient.get"https://na.api.pvp.net/api/lol/na/v1.3/game/by-summoner/#{@id}/recent?api_key=45ff6a4c-f994-420d-a018-189db4cb494e")
   end
 
   def champ_dir
-    @champ_dir = JSON.parse(RestClient.get"https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion?api_key=45ff6a4c-f994-420d-a018-189db4cb494e")
-    @champ_image = @champ_dir['data']
+    @championId = match_history['games'][0]['championId']
+    @champ_dir = JSON.parse(RestClient.get"https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion/#{@championId}?api_key=45ff6a4c-f994-420d-a018-189db4cb494e")
   end
+
+  def game_stats
+    match_history['games'][0]['stats']
+  end
+
+  def items
+    item1 = game_stats['item0']
+  end
+
+
+  def champ_icon
+    champ_dir['key']
+  end
+
+
 
   def summoner_stats
     masteries
     match_history
-    @name = @stats.keys.first
-    @match_info = @match_history['matches']
-    @participants =@match_info[0]['participants'][0]
-    @game_stats = @participants['stats']
+    @level = game_stats['level']
+    items
+    @champ_icon = champ_icon
+raise
+
+    #@name = @stats.keys.first
+    #@match_info = @match_history['matches']
+    #@participants =@match_info[0]['participants'][0]
+    #@game_stats = @participants['stats']
 
   end
 
