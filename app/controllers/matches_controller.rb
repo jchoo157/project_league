b2beefd7f744776df7efa3f23f40dcf816d7604f
Johnny <jchoo157@gmail.com>
2015-09-09 15:15:21 -0400

    fixed match_controller

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -6,14 +6,6 @@ class MatchesController < ApplicationController
 
   def show
    @user = User.new(username: user_params[:name])
-      @user.match_history.map do |match|
-      @game_type = match[:matchType]
-      @game_time = match[:matchDuration]
-      match[:participants].each do |peeps|
-        @champ_id = peeps[:championId]
-        @stats = peeps[:stats]
-      end
-    end
   end
 
   private
