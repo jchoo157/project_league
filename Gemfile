db2950696a764be8180bae1e0c8bfe51b7841455
Johnny <jchoo157@gmail.com>
2015-09-15 07:37:03 -0400

    refactor and add data to match history partial

--- Gemfile
+++ Gemfile
@@ -35,7 +35,9 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
-
+group :test do
+  gem 'webmock'
+end
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/helpers/matches_helper.rb
+++ app/helpers/matches_helper.rb
@@ -1,4 +1,5 @@
 module MatchesHelper
+attr_reader :level
 
   def list_of_champions
     @list ||= JSON.parse(ChampionApi.new.get_list_of_champions, symbolize_names: true)
@@ -6,5 +7,13 @@ module MatchesHelper
 
   def get_champion_name(id:)
     list_of_champions[:data].select { |_, info| info[:id] == id }.first.first.to_s
+  end
+
+  def item_img_url
+    'http://ddragon.leagueoflegends.com/cdn/5.2.1/img/item/'
+  end
+
+  def champ_icon_url
+    'http://ddragon.leagueoflegends.com/cdn/5.2.1/img/champion/'
   end
 end
