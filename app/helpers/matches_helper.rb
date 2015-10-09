3803d06230474a3bd3ae3ecfd093e10787e6e8f4
Johnny <jchoo157@gmail.com>
2015-10-09 16:04:58 -0400

    add time conversion and gold conversion for the match history

--- app/helpers/matches_helper.rb
+++ app/helpers/matches_helper.rb
@@ -22,7 +22,60 @@ module MatchesHelper
     'http://ddragon.leagueoflegends.com/cdn/5.2.1/img/spell/'
   end
 
+  def time(seconds)
+    minutes = 0
+    while seconds >= 60
+      seconds -= 60
+      minutes += 1
+    end
+    if seconds < 10
+      seconds_s = '0' + seconds.to_s
+    else
+      seconds_s = seconds.to_s
+    end
+    return minutes.to_s + ':' + seconds_s
+  end
+
+  def gold(amount)
+    k = 0
+    while amount >= 1000
+      amount -= 1000
+      k += 1
+    end
+    d = 0
+    while amount >= 100
+      amount -= 100
+      d += 1
+    end
+    return k.to_s + '.' + d.to_s + 'k'
+  end
+
+  def game_type(game_mode)
+    if game_mode == 'MATCHED_GAME'
+      'Ranked 5v5'
+    end
+  end
+
+  def teams(team)
+    t1 = []
+    t2 = []
+      while team <= 5
+        t1 += team
+      while team > 5
+        t2 += team
+      end
+    end
+  end
+
+  def result(boolean)
+    if boolean == 'true'
+      'Win'
+    else
+      'Loss'
+    end
+  end
 end
+
 
 
 
