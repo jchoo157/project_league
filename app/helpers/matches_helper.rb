1f2d03ff48c0aa765dab60fe8cbb640caa24f89f
Johnny <jchoo157@gmail.com>
2015-10-28 05:31:19 -0400

    add the ability to select a champion to view stats

--- app/helpers/matches_helper.rb
+++ app/helpers/matches_helper.rb
@@ -2,10 +2,6 @@ module MatchesHelper
 
   attr_reader :level
 
-  def list_of_champions
-    @list ||= JSON.parse(ChampionApi.new.get_list_of_champions, symbolize_names: true)
-  end
-
   def get_champion_name(id:)
     list_of_champions[:data].select { |_, info| info[:id] == id }.first.first.to_s
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
 
   devise_for :accounts
+
   root 'static_pages#home'
   get 'home' => 'static_pages#home'
   get 'help' => 'static_pages#help'
   get 'about' => 'static_pages#about'
   get 'champions' => 'static_pages#champions'
+
+
+  get 'champion_info' => 'static_pages#champion_info'
 
   resource :match
 
