4ee1d13ee3c9be49a0bb6087e24e59c88b1a566a
David Quach <davidnquach@gmail.com>
2015-11-06 23:41:03 -0500

    Remove commented out code

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,10 +5,6 @@ class StaticPagesController < ApplicationController
   def about
   end
 
-  def matches
-    @user = User.new(username: user_params[:name])
-  end
-
   def help
   end
 
@@ -16,11 +12,5 @@ class StaticPagesController < ApplicationController
   end
 
   def champion_info
-  end
-
-  private
-
-  def user_params
-    params.require(:summoner).permit(:name)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,6 @@ class User
     @username = properties[:username]
     @api = SummonerApi.new
     @match_info = MatchInfo.new(summoner_id: id)
-    # @id = get_id(api: SummonerApi.new)
-    # @match_history = get_match_histories(api: MatchHistoryApi.new)
   end
 
   def id
@@ -22,19 +20,7 @@ class User
   def parse(data)
     JSON.parse(data, symbolize_names: true)
   end
-  # def get_all_match_ids(api:)
-  #   info = parse api.get_match_list(for_summoner_id: id)
-  #   data = info[:matches]
-  #   data.map { |match| match[:matchId] }
-  # end
-  #
-  # def get_match_histories(api:, count: 5)
-  #   ids = get_all_match_ids(api: MatchListApi.new)
-  #   data = []
-  #   ids[0...count].each { |id| data << parse(api.get_match_history(for_id: id)) }
-  #   data
-  # end
-  #
+
   def get_id
     info = parse api.get_summoner_info(by_name: username)
     info[username.to_sym][:id]
